825d6b4bb70acbb1a4b9a045c1c947205fb3fcfc
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2024-02-01 00:18:31 +0100

    fix cors

--- weeks-backend/config/cors.ts
+++ weeks-backend/config/cors.ts
@@ -44,12 +44,15 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://weeks.likablehair.it'
-  ],
+  origin: function(origin) {
+    // [
+    //   'http://localhost:3000',
+    //   'http://localhost:5173',
+    //   'http://127.0.0.1:5173',
+    //   'https://weeks.likablehair.it'
+    // ]
+    return origin
+  },
 
   /*
   |--------------------------------------------------------------------------
